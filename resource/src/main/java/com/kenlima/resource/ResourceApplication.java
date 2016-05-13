ba44205f09bf9a35de578cfca7cc3071f8488735
jwlee <kenlima@wemakeprice.com>
2016-05-14 08:52:22 +0900

    oauth2 적용완료

--- resource/src/main/java/com/kenlima/resource/ResourceApplication.java
+++ resource/src/main/java/com/kenlima/resource/ResourceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +15,10 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
-public class ResourceApplication extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class ResourceApplication {
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.authorizeRequests().anyRequest().authenticated();
-    }
 
     @RequestMapping("/")
     public Map<String, Object> home() {
